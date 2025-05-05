90550a7c89bae5d25e216c59d24167ef3a7204ce
Guilherme Vieira Brito <gvieirabrito390@gmail.com>
2025-05-05 10:30:15 -0300

    Add files via upload

---
+++ Lista 03 IP/Lista 03 Q18.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main() {
+ rows, cols := 10,10
+    
+ for i:=0;i<rows;i++{
+    for j:=0;j<cols;j++{
+        
+        if i == j {
+        fmt.Printf("[%d][%d] ",i, j)
+        
+        }
+ }   
+   fmt.Println()
+ }
+ 
+}
---
+++ Lista 03 IP/lista 03 q 17 real.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+    rows, cols := 10,10
+    
+ for i:=0;i<rows;i++{
+    for j:=0;j<cols;j++{
+        fmt.Printf("[%d][%d] ",i, j)
+        
+     
+ }   
+   fmt.Println()
+ }
+ 
+}
