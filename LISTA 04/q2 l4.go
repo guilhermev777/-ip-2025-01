bba6fd91eb57736ad5b39cabe0d374555b559940
Guilherme Vieira Brito <gvieirabrito390@gmail.com>
2025-06-14 12:00:44 -0300

    Update q2 l4.go

--- LISTA 04/q2 l4.go
+++ LISTA 04/q2 l4.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// Declaração dos vetores
+	
 	var vetor1 [10]int
 	var vetor2 [5]int
 	var paresSoma []int
 	var imparesSoma []int
 
-	// Preenchimento do primeiro vetor
+	
 	fmt.Println("Digite os 10 números do primeiro vetor:")
 	for i := 0; i < 10; i++ {
 		fmt.Printf("Elemento %d: ", i+1)
@@ -23,13 +23,13 @@ func main() {
 		fmt.Scan(&vetor2[i])
 	}
 
-	// Calcula a soma total do segundo vetor
+	
 	somaSegundo := 0
 	for _, num := range vetor2 {
 		somaSegundo += num
 	}
 
-	// Processa os números do primeiro vetor
+	
 	for _, num := range vetor1 {
 		if num%2 == 0 { // Se for par
 			paresSoma = append(paresSoma, num+somaSegundo)
@@ -38,7 +38,7 @@ func main() {
 		}
 	}
 
-	// Exibe os resultados
+	
 	fmt.Println("\nPrimeiro vetor resultante (pares + soma do segundo vetor):", paresSoma)
 	fmt.Println("Segundo vetor resultante (ímpares + soma do segundo vetor):", imparesSoma)
 }
