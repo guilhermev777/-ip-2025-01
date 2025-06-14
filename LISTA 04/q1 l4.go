9c892b3e189069ab981ece4dd4d30b76c12b9ea6
Guilherme Vieira Brito <gvieirabrito390@gmail.com>
2025-06-14 12:01:05 -0300

    Update q1 l4.go

--- LISTA 04/q1 l4.go
+++ LISTA 04/q1 l4.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// Criar um vetor com 10 elementos
+	
 	var numeros [10]int
 	var contador int
 
-	// Preencher o vetor
+	
 	fmt.Println("Digite 10 números inteiros:")
 	for i := 0; i < 10; i++ {
 		fmt.Printf("Número %d: ", i+1)
 		fmt.Scan(&numeros[i])
 	}
 
-	// Verificar números maiores que 50 e suas posições
+	
 	fmt.Println("\nNúmeros maiores que 50 e suas posições:")
 	for indice, valor := range numeros {
 		if valor > 50 {
@@ -23,7 +23,7 @@ func main() {
 		}
 	}
 
-	// Se não houver números maiores que 50
+	
 	if contador == 0 {
 		fmt.Println("Não existem números maiores que 50 no vetor.")
 	}
